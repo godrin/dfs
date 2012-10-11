daead891f622b3fafb26ef638985bd426a8f22e0
David Kamphausen <david.kamphausen@web.de>
2012-10-11 22:02:55 +0200

    reading from file

--- dfs.js
+++ dfs.js
@@ -1,11 +1,11 @@
 var file=require('./file.js');
 
-var storage=new file.storage("test.txt",1);
+var storage=new file.storage("test.txt",2);
 
 
 console.log("FILE..");
 console.log(file);
 
-storage.read(1,function(err, bytesRead, buffer){
-	console.log(err, bytesRead, buffer);
+storage.read(0,function(err, bytesRead, buffer){
+	console.log("READ callback",err, bytesRead, buffer,buffer.toString('utf8',0,bytesRead));
 });
